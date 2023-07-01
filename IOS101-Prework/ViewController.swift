d2c3bac45624925536fce40d07112e34d87b77a7
Lana <do.ng@northeastern.edu>
2023-07-01 14:44:00 -0700

    additional features

--- IOS101-Prework/ViewController.swift
+++ IOS101-Prework/ViewController.swift
@@ -8,14 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x:75,y:250, width: 250,height:250)
         
+        getRandomPhoto()
     }
 
-
+    @IBOutlet weak var nameLabel: UILabel!
     @IBAction func changeBackgroundColor(_ sender: UIButton) {
         func changeColor() -> UIColor{
 
@@ -25,8 +35,24 @@ class ViewController: UIViewController {
 
                 return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
             }
+
         let randomColor = changeColor()
             view.backgroundColor = randomColor
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 18)
+        nameLabel.textColor = UIColor.systemPink
+        getRandomPhoto()
+        
     }
+    
+    func getRandomPhoto() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data:data)
+    }
+    
 }
+
 
